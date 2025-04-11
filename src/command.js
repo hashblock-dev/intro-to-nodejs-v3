d2f2c20aba1a46a629e1dfab660acfbedf758859
Cedric <cedric@hashblock.dev>
2025-04-11 23:45:18 +0200

    feat: add create note command

--- src/command.js
+++ src/command.js
@@ -2,8 +2,23 @@ import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 
 yargs(hideBin(process.argv))
-  .command("curl <url>", "fetch the contents of the URL", () => {
-    console.info(process.argv);
+  .command(
+    "new <note>",
+    "Create a new note",
+    (yargs) => {
+      return yargs.positional("note", {
+        describe: "The content of the note to create",
+        type: "string",
+      });
+    },
+    (argv) => {
+      console.log(argv.note);
+    }
+  )
+  .options("tags", {
+    alias: "t",
+    describe: "tags to add to the note",
+    type: "string",
   })
   .demandCommand(1)
   .parse();
